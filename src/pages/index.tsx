fe90da5589e58093a682ddd1db6223b3463bc3bb
Sanda Mandresy <98638893+Sanmandresy@users.noreply.github.com>
2023-06-15 10:25:41 +0300

    feat: display server time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 /**
   Calculates the time difference between the server time and client time.
@@ -7,14 +8,67 @@ import { useRouter } from "next/router";
   @param {Date} clientTime - The client time.
   @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
 */
-const calculateTimeDifference = (server: Date, client: Date) => {};
 
+const calculateTimeDifference = (server: Date, client: Date) => {
+  // convert the IsoString to Date
+  const serverTime = new Date(server);
+  // Calculate the difference in milliseconds
+  const diffInMilliseconds = Math.abs(serverTime.getTime() - client.getTime());
 
-export default function Home() {
+  // Convert the difference in milliseconds to days, hours, minutes, and seconds
+  const days = Math.floor(diffInMilliseconds / 86400000);
+  const hours = Math.floor((diffInMilliseconds % 86400000) / 3600000);
+  const minutes = Math.floor((diffInMilliseconds % 3600000) / 60000);
+  const seconds = Math.floor((diffInMilliseconds % 60000) / 1000);
+  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+};
+
+const displayFullDate = (date: string) => {
+  // Format: DD-MM-AAAA HH:mm
+  // Split the string to get the date
+  const data = date.split("T")[0];
+  const days = data.split("-")[2];
+  const month = data.split("-")[1];
+  const year = data.split("-")[0];
+
+  //Split the string to get the time
+  const time = date.split("T")[1];
+  const hours = time.split(":")[0];
+  const minutes = time.split(":")[1];
+
+  return `${days}-${month}-${year} ${hours}:${minutes}`;
+};
+
+type ServerTime = {
+  server: Date;
+};
+
+export const getServerSideProps = async () => {
+  const server = new Date().toISOString();
+  return {
+    props: {
+      server,
+    },
+  };
+};
+
+export default function Home({ server }: ServerTime) {
+  const [client, setClient] = useState<Date | null>(null);
+  const [timeDifference, setTimeDifference] = useState<string>("");
   const router = useRouter();
   const moveToTaskManager = () => {
     router.push("/tasks");
-  }
+  };
+  useEffect(() => {
+    setClient(new Date());
+  }, []);
+
+  useEffect(() => {
+    if (client) {
+      setTimeDifference(calculateTimeDifference(server, client));
+    }
+  }, [client, server]);
+
   return (
     <>
       <Head>
@@ -29,13 +83,14 @@ export default function Home() {
           {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
           <p>
             Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">
+              {displayFullDate(server.toString())}
+            </span>
           </p>
 
           {/* Display here the time difference between the server side and the client side */}
           <p>
-            Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            Time diff: <span className="serverTime">{timeDifference}</span>
           </p>
         </div>
 
